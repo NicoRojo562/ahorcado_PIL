b9e77317aefd8a03374b2d6c07267b38a02fbf72
NicoRojo562 <2000262@ucc.edu.ar>
2022-09-17 21:59:26 -0300

    comienzo del main

--- ahorcado.py
+++ ahorcado.py
@@ -1,16 +1,7 @@
 import random
 
-print("------BIENVENIDO AL JUEGO DEL AHORCADO-------\n")
 
-nickname = input("Ingrese su Nickname: ")
-
-print(nickname)
-
-print("\n¡Hola " + nickname + "!\n")
-print("Este juego tiene 3 Niveles:\n\t1-Easy\n \t2-Medium\n \t3-Hard")
-
-
-
+#Funcion q elige el nivel
 def elegir_nivel():
     nivel = print(int(input("Elije tu nivel para jugar: ")))
     if nivel == 1:
@@ -23,21 +14,37 @@ def elegir_nivel():
         print("Opción incorrecta")
     return nivel
 
+
+#encripta la palabra
+
+def encripted_word(word):
+    for char in word:
+        
+        encripted_random_word = print(char.replace(char," * "), end='')
+     
+    return encripted_random_word
+
+
+
+#main 
+print("------BIENVENIDO AL JUEGO DEL AHORCADO-------\n")
+
+nickname = input("Ingrese su Nickname: ")
+
+print(nickname)
+
+print("\n¡Hola " + nickname + "!\n")
+print("Este juego tiene 3 Niveles:\n\t1-Easy\n \t2-Medium\n \t3-Hard")
+
 elegir_nivel()
 
 words_array=["Python","Class","Technology","Language","Coding"]
 
-def random_word(array):
-    
-    """selects a random word from a List and changes 
-                all char in the word with a '*' """
-    
-    word = random.choice(array) 
-     
-    for char in word:
-        
-        encripted_random_word = print(char.replace(char," _ "), end='')
-     
-    return encripted_random_word
+word= random.choice(words_array)
 
-random_word(words_array)
+yword=str(len(word))
+
+print("su palabra tiene " + yword +" letras")
+
+encripted_word(word)
+
