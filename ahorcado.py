2b88837414f585e89803dff903fadd6dfa0d0f53
Francisco <francisco2000fran@gmail.com>
2022-09-18 10:26:32 -0300

    Se buscó implementar las clases a nuestro proyecto

--- ahorcado.py
+++ ahorcado.py
@@ -1,50 +1,132 @@
+# Proyecto PIL - Juego Ahorcado
+# Importamos el modulo random y clases
+import tematicas
 import random
 
 
-#Funcion q elige el nivel
+# Cargamos las palabras en las clases
+def cargar_clases():
+    frutas = ("naranja", "manzana", "banana", "kiwi", "pomelo")
+    personas_celebres = ("Shakira", "Messi")
+    mundo = ("Jamaica", "Inglaterra", "Argentina")
+    provincias = ("San juan", "Jujuy", "Cordoba")
+    car = tematicas.Tematica(frutas, personas_celebres, mundo, provincias)
+    return car
+
+
+# Función de validación
+def validar_entre(inf, sup, mensaje):
+    opcion = int(input(mensaje))
+    while opcion < inf or opcion > sup:
+        print("Error! Debe elegir un valor que valido...")
+        opcion = int(input(mensaje))
+    return opcion
+
+
+# Funcion que elige el nivel
+def elegir_nivel1():
+    print("-" * 80)
+    print("Elección de Dificultad")
+    print(f"1. Facil" + " |2. Medio" + " |3. Dificil")
+    opcion = validar_entre(1, 3, "Ingrese el numero de dificultad: ")
+    return opcion
+    
+
 def elegir_nivel():
-    nivel = print(int(input("Elije tu nivel para jugar: ")))
+    nivel = int(input("Elije tu nivel para jugar: "))
     if nivel == 1:
         pass
     elif nivel == 2:
         pass
-    elif nivel ==3:
+    elif nivel == 3:
         pass
     else:
         print("Opción incorrecta")
     return nivel
 
 
-#encripta la palabra
+# Elegimos la tematica a utilizar en el juego
+def elegir_tematica():
+    print("Elección de Tematica")
+    print("Elija entre las siguientes temáticas: ")
+    print("1. Frutas\n" + "2. Personas Celebres\n" + "3. Mundo\n" + "4. Provincias\n")
+    eleccion = validar_entre(1, 4, "Opcion elegida: ")
+    return eleccion
 
+
+# Encripta la palabra
 def encripted_word(word):
     for char in word:
-        
-        encripted_random_word = print(char.replace(char," * "), end='')
-     
+        encripted_random_word = print(char.replace(char, " * "), end='')
+
     return encripted_random_word
 
 
+# Main Principal
+def principal():
+    print("-" * 80)
+    print("------> BIENVENIDO AL JUEGO DEL AHORCADO <-------")
+    print("-" * 80)
 
-#main 
-print("------BIENVENIDO AL JUEGO DEL AHORCADO-------\n")
+    # Pedimos al usuario que ingrese su nombre...
+    nickname = input("Ingrese su Nickname: ")
 
-nickname = input("Ingrese su Nickname: ")
+    # Saludamos al usuario con un mensaje simple...
+    print("\n¡Hola " + nickname + "!\n")
 
-print(nickname)
+    # Creamos esta variable para validar luego, si el usuario ingreso en la opcion 1 para poder continuar el juego
+    tematica = False
+    tematica_elegida = None
 
-print("\n¡Hola " + nickname + "!\n")
-print("Este juego tiene 3 Niveles:\n\t1-Easy\n \t2-Medium\n \t3-Hard")
+    # Desarrollamos menu de opciones...
+    opcion = -1
+    while opcion != 0:
+        print("MENÚ DE OPCIONES")
+        print("1. Elegir tematica")
+        print("2. Jugar")
+        print("0. Salir")
+        opcion = int(input("Ingrese la opcion elegida: "))
 
-elegir_nivel()
+        # En esta opcion se activa la bandera "tematica" para notificar que se eligió una tematica para el ahorcado
+        if opcion == 1:
+            tematica = True
 
-words_array=["Python","Class","Technology","Language","Coding"]
+            # En esta opcion pensaba hacer elegir la tematica para usar luego en la opcion 2...
+            tematica_elegida = elegir_tematica()
 
-word= random.choice(words_array)
+        elif tematica == False:
+            print("-" * 80)
+            print("Debe ingresar primero la temática en la opcion 1...")
 
-yword=str(len(word))
+        # En opcion 2, validamos que la opcion sea igual a 2 y que la bandera tematica este prendida (valiendo True)
+        # esto nos indicará que el usuario ya elgió una tematica para jugar...
+        elif opcion == 2 and tematica:
+            nivel_elegido = elegir_nivel1()
+            if nivel_elegido == 1:
+                # jugamos nivel facil
+                pass
+            elif nivel_elegido == 2:
+                # jugamos nivel medio
+                pass
+            elif nivel_elegido == 3:
+                # jugamos nivel dificil
+                pass
+        elif opcion == 0:
+            print("Gracias por jugar con nosotros " + nickname + " nos vemos la próxima!")
+        else:
+            print("Opción incorrecta! Vuelva a intentar " + nickname + " por favor!")
 
-print("su palabra tiene " + yword +" letras")
+    words_array = ["Python", "Class", "Technology", "Language", "Coding"]
 
-encripted_word(word)
+    word = random.choice(words_array)
 
+    yword = str(len(word))
+
+    print("su palabra tiene " + yword + " letras")
+
+    encripted_word(word)
+
+
+# Ejecutamos el programa
+if __name__ == '__main__':
+    principal()
---
+++ tematicas.py
@@ -0,0 +1,8 @@
+# Este modulo nos servirá para utilizar las tematicas de nuestro ahorcado
+class Tematica:
+    def __init__(self, frutas, personas_celebres, mundo, provincias):
+        self.frutas = frutas
+        self.personas_celebres = personas_celebres
+        self.mundo = mundo
+        self.provincias = provincias
+
